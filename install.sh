53b9693d8303b6fd6a65d9f1061849c6418f6563
Giuseppe <p3pp1n0@gmail.com>
2019-11-15 21:31:32 +0100

    Proprer mount for blockchain data, corrected install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ mkdir -p wavesnode-data
 cd wavesnode-data
 wget http://blockchain.wavesnodes.com/blockchain_last.tar
 tar xvf blockchain_last.tar
+echo "Cleaning up.."
+rm -rf blockchain_last.tar
 cd ..
-echo "Copying config over"
-cp config.yml ./blockchain-postgres-sync
 echo "Run docker-compose up -d to get running!"
